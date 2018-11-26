ed90123a7fef7f9dfcfa0fa0be5e7b0fc398e798
Christian Parpart <christian@ethereum.org>
2018-11-27 00:18:22 +0100

    liblangutil: refactors SourceReferenceFormatter error formatting for pretty and colored output.
    * Refactors output format in a way it is (or should at least be) more readable.
      (NB.: As source of inspiration, I chose the rustc compiler output.)
    * Adds color support to the stream output.
    * Also improves multiline source formatting
      (i.e. truncating too long lines, like done with single lines already)

--- liblangutil/SourceReferenceFormatter.cpp
+++ liblangutil/SourceReferenceFormatter.cpp
@@ -23,10 +23,22 @@
 #include <liblangutil/SourceReferenceFormatter.h>
 #include <liblangutil/Scanner.h>
 #include <liblangutil/Exceptions.h>
+#include <libdevcore/termcolor.h>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 using namespace dev;
 using namespace langutil;
+
+namespace
+{
+inline ostream& frameColor(ostream& os) { return os << termcolor::bold << termcolor::blue; };
+inline ostream& messageColor(ostream& os) { return os << termcolor::bold << termcolor::white; };
+inline ostream& errorColor(ostream& os) { return os << termcolor::bold << termcolor::red; };
+inline ostream& diagColor(ostream& os) { return os << termcolor::bold << termcolor::yellow; };
+inline ostream& highlightColor(ostream& os) { return os << termcolor::yellow; };
+}
 
 void SourceReferenceFormatter::printSourceLocation(SourceLocation const* _location)
 {
@@ -38,56 +50,73 @@ void SourceReferenceFormatter::printSourceLocation(SourceReference const& _ref)
 	if (_ref.startLine < 0)
 		return; // Nothing we can print here
 
+	int const leftpad = static_cast<int>(log10(max(_ref.startLine, 1))) + 1;
+
+	// line 0: source name
+	m_stream << string(leftpad, ' ') << frameColor << "--> " << termcolor::reset;
+	m_stream << _ref.name << ":" << (_ref.interestLine + 1) << ":" << (_ref.interestColumn + 1) << ": ";
+
+	if (!_ref.description.empty())
+		m_stream << messageColor << _ref.description << termcolor::reset << '\n';
+	else
+		m_stream << '\n';
+
 	if (_ref.startLine == _ref.endLine)
 	{
-		string const& line = _ref.line;
+		// line 1:
+		m_stream << string(leftpad, ' ') << frameColor << " |" << termcolor::reset << '\n';
 
-		m_stream << line << endl;
+		// line 2:
+		m_stream << frameColor << _ref.interestLine << " | " << termcolor::reset
+				 << _ref.line.substr(0, _ref.startColumn)
+				 << highlightColor << _ref.line.substr(_ref.startColumn, _ref.locationLength) << termcolor::reset
+				 << _ref.line.substr(_ref.endColumn) << '\n';
 
+		// line 3:
+		m_stream << string(leftpad, ' ') << frameColor << " | " << termcolor::reset;
 		for_each(
-			line.cbegin(),
-			line.cbegin() + _ref.startColumn,
+			_ref.line.cbegin(),
+			_ref.line.cbegin() + _ref.startColumn,
 			[this](char const& ch) { m_stream << (ch == '\t' ? '\t' : ' '); }
 		);
-		m_stream << "^";
-		if (_ref.endColumn > _ref.startColumn + 2)
-			m_stream << string(_ref.endColumn - _ref.startColumn - 2, '-');
-		if (_ref.endColumn > _ref.startColumn + 1)
-			m_stream << "^";
-		m_stream << endl;
+		m_stream << diagColor << string(_ref.locationLength, '^') << termcolor::reset << '\n';
 	}
 	else
-		m_stream <<
-			_ref.line <<
-			endl <<
-			string(_ref.startColumn, ' ') <<
-			"^ (Relevant source part starts here and spans across multiple lines)." <<
-			endl;
-}
+	{
+		// line 1:
+		m_stream << string(leftpad, ' ') << frameColor << " |" << termcolor::reset << '\n';
 
-void SourceReferenceFormatter::printSourceName(SourceReference const& _ref)
-{
-	if (_ref.interestLine != -1)
-		m_stream << _ref.name << ":" << (_ref.interestLine + 1) << ":" << (_ref.interestColumn + 1) << ": ";
+		// line 2:
+		m_stream << frameColor << _ref.interestLine << " | " << termcolor::reset
+				 << _ref.line.substr(0, _ref.startColumn)
+				 << highlightColor << _ref.line.substr(_ref.startColumn) << termcolor::reset << '\n';
+
+		// line 3:
+		m_stream << string(leftpad, ' ') << frameColor << " | " << termcolor::reset
+				 << string(_ref.startColumn, ' ')
+				 << diagColor << "^ (Relevant source part starts here and spans across multiple lines)."
+				 << termcolor::reset << '\n';
+	}
+
+	m_stream << '\n';
 }
 
 void SourceReferenceFormatter::printExceptionInformation(dev::Exception const& _error, std::string const& _category)
 {
-	printExceptionInformation(SourceReferenceExtractor::extract(_error, _category));
-}
+	auto const ref = SourceReferenceExtractor::extract(_error, _category);
 
-void SourceReferenceFormatter::printExceptionInformation(SourceReferenceExtractor::Message const& _msg)
-{
-	printSourceName(_msg.primary);
+	// exception header line
+	m_stream << errorColor << ref.category;
+	if (string const* description = boost::get_error_info<errinfo_comment>(_error))
+		m_stream << messageColor << ": " << *description << endl;
+	else
+		m_stream << termcolor::reset << '\n';
 
-	m_stream << _msg.category << ": " << _msg.primary.description << endl;
+	if (!ref.primary.startLine || ref.primary.name.empty())
+		m_stream << '\n';
 
-	printSourceLocation(_msg.primary);
+	printSourceLocation(ref.primary);
 
-	for (auto const& ref : _msg.secondary)
-	{
-		printSourceName(ref);
-		m_stream << ref.description << endl;
-		printSourceLocation(ref);
-	}
+	for (auto const& secondary : ref.secondary)
+		printSourceLocation(secondary);
 }
